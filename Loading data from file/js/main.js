1e21b36bbb2ef871948213d02ae7d58ee22e310d
Alex Vargas <71559653+AlejandroVargas0218841@users.noreply.github.com>
2022-06-01 19:40:43 -0500

    Update main.js

--- Loading data from file/js/main.js
+++ Loading data from file/js/main.js
@@ -1,24 +1,25 @@
 var svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
 	.attr("height", 400);
-	
 
 d3.json("data/ages.json").then((data)=> {
-	data.forEach((d) => {
+	data.forEach((d)=>{
 
 		d.age = +d.age;
-		
+
 	});
-	console.log(data);
 
-		var circles=svg.selectAll("circles").data(data);
-	circles.enter()
-		.append("circles")
-			.attr("cx",(d,i)=>{return (i*50)+20;})
-			.attr("cy",50)
-			.attr("r", (d)=>{return d.age;})
-			.attr("fill",(d)=>{if(d.age>10){return "orange";} else{return "blue";}});
-	}).catch((error)=>{
-		console.log(error);
+    console.log(data);
+
+	var circles=svg.selectAll("circle").data(data);
+
+    circles.enter()
+        .append("circle")
+	    .attr("cx",(d,i)=>{return (i*50)+20;})
+	    .attr("cy",50)
+	    .attr("r", (d)=>{return d.age;})
+	    .attr("fill",(d)=>{if(d.age>10){return "blue";} else{return "orange";}});
+
+}).catch((error)=>{
+    console.log(error);
 });
-
